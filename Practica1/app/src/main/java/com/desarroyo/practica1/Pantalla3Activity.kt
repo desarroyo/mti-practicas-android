403e6c7e762a1113c8408a667883bcc724df8397
desarroyo <chevy.arroyo@gmail.com>
2019-06-20 14:07:21 -0500

    Practica 1 - Layouts

---
+++ Practica1/app/src/main/java/com/desarroyo/practica1/Pantalla3Activity.kt
@@ -0,0 +1,12 @@
+package com.desarroyo.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla3)
+    }
+}
